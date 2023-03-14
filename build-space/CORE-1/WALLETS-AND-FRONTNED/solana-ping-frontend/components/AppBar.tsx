0714da0f027e5e531f887d63cc10f9bbcde970ce
MirzaMuhammadBaig <muhammadbaig090@gmail.com>
2023-03-14 08:55:00 +0500

    🔌 Connecting to wallets Solana-Adapter-Wallet

---
+++ build-space/CORE-1/WALLETS-AND-FRONTNED/solana-ping-frontend/components/AppBar.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { FC } from 'react'
+import styles from '../styles/Home.module.css'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import Image from 'next/image'
+
+export const AppBar: FC = () => {
+    return (
+        <div className={styles.AppHeader}>
+            <Image src="/solanaLogo.png" height={30} width={200} />
+            <span>Wallet-Adapter Example</span>
+            <WalletMultiButton />
+        </div>
+    )
+}
---
+++ build-space/CORE-1/WALLETS-AND-FRONTNED/solana-ping-frontend/pages/_app.tsx
@@ -0,0 +1,44 @@
+import React, { useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
+import "../styles/globals.css";
+import "../styles/Home.module.css";
+
+function MyApp({ Component, pageProps }) {
+  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can provide a custom RPC endpoint here
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter(), new GlowWalletAdapter()],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
+
+export default MyApp;
